c6cc01a2a653022d36a0f1941db39d49d4972c88
ken <kenchen@pneumasoftware.com>
2019-07-10 17:59:00 +0800

    Implement routing.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "react-bootstrap/Button";
+import { Home } from "./components/Home";
+import { Department } from "./components/Department";
+import { Employee } from "./components/Employee";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="Container">
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/department" component={Department} />
+          <Route path="/employee" component={Employee} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
