2ed04518f5703e57373ce6c51264f19156b7dc4c
ken <kenchen@pneumasoftware.com>
2019-07-10 18:29:42 +0800

    Add navigation bar

--- src/App.js
+++ src/App.js
@@ -5,12 +5,20 @@ import Button from "react-bootstrap/Button";
 import { Home } from "./components/Home";
 import { Department } from "./components/Department";
 import { Employee } from "./components/Employee";
+import { Navigation } from "./components/Navigation";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="Container">
+        <h3 className="m-3 d-flex justify-content-center">
+          React JS with Web api demo
+        </h3>
+        <h5 className="m-3 d-flex justify-content-center">
+          Employee Management Portal
+        </h5>
+        <Navigation />
         <Switch>
           <Route path="/" component={Home} exact />
           <Route path="/department" component={Department} />
