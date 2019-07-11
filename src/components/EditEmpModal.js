8aa7752a1a01c07f52199ac04bdaabf2aa01cc3b
ken <kenchen@pneumasoftware.com>
2019-07-11 17:23:27 +0800

    Employee

---
+++ src/components/EditEmpModal.js
@@ -0,0 +1,151 @@
+import React, { Component } from "react";
+import { Modal, Button, Row, Col, Form, FormGroup } from "react-bootstrap";
+import SnackBar from "@material-ui/core/SnackBar";
+import IconButton from "@material-ui/core/IconButton";
+
+export class EditEmpModal extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { snackbaropen: false, snackbarmsg: "" };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  snackbarClose = event => {
+    this.setState({ snackbaropen: false });
+  };
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    fetch("https://localhost:44306/api/employee", {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        EmployeeID: null,
+        EmployeeName: event.target.EmployeeName.value,
+        Department: event.target.Department.value,
+        MailID: event.target.MailID.value,
+        DOJ: event.target.DOJ.value
+      })
+    })
+      .then(response => response.json())
+      .then(
+        result => {
+          //alert(result);
+          this.setState({ snackbaropen: true, snackbarmsg: result });
+        },
+        error => {
+          alert("error");
+        }
+      );
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <SnackBar
+          anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+          open={this.state.snackbaropen}
+          autoHideDuration={3000}
+          onClose={this.snackbarClose}
+          message={<span id="message-id">{this.state.snackbarmsg}</span>}
+          action={[
+            <IconButton
+              key="close"
+              arial-label="Close"
+              color="inherit"
+              onClick={this.snackbarClose}
+            >
+              x
+            </IconButton>
+          ]}
+        />
+
+        <Modal
+          {...this.props}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Add Empartment
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Row>
+              <Col sm={6}>
+                <Form onSubmit={this.handleSubmit}>
+                  <Form.Group controlId="EmployeeID">
+                    <Form.Label>Employee ID</Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="EmployeeID"
+                      required
+                      disabled
+                      defaultValue={this.props.empid}
+                      placeholder="Employee ID"
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="EmployeeName">
+                    <Form.Label>Employee Name</Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="EmployeeName"
+                      required
+                      defaultValue={this.props.empname}
+                      placeholder="Employee Name"
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="Department">
+                    <Form.Label>Department</Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="Department"
+                      required
+                      defaultValue={this.props.department}
+                      placeholder="Department"
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="MailID">
+                    <Form.Label>Mail ID</Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="MailID"
+                      required
+                      defaultValue={this.props.mailid}
+                      placeholder="Mail ID"
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="DOJ">
+                    <Form.Label>Date of Join</Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="DOJ"
+                      defaultValue={this.props.doj}
+                      placeholder="DOJ"
+                    />
+                  </Form.Group>
+                  <Form.Group>
+                    <Button variant="primary" type="submit">
+                      Update Employee
+                    </Button>
+                  </Form.Group>
+                </Form>
+              </Col>
+            </Row>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="danger" onClick={this.props.onHide}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
+}
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,10 +1,120 @@
 import React, { Component } from "react";
+import { Table, Button, ButtonToolbar, ButtonGroup } from "react-bootstrap";
+import { AddEmpModal } from "./AddEmpModal";
+import { EditEmpModal } from "./EditEmpModal";
 
 export class Employee extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { emps: [], addModalShow: false, editModalShow: false };
+  }
+
+  componentDidMount() {
+    this.refrshList();
+  }
+
+  componentDidUpdate() {
+    this.refrshList();
+  }
+
+  refrshList() {
+    fetch("https://localhost:44306/api/employee").then(response =>
+      response.json().then(data => {
+        this.setState({ emps: data });
+      })
+    );
+  }
+
+  deleteEmp(empid) {
+    if (window.confirm("Are you sure?")) {
+      fetch("https://localhost:44306/api/employee/" + empid, {
+        method: "DELETE",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        }
+      });
+    }
+  }
+
   render() {
+    const { emps, empid, empname, department, mailid, doj } = this.state;
+    let addModalClose = () => this.setState({ addModalShow: false });
+    let editModalClose = () => this.setState({ editModalShow: false });
+
     return (
       <div className="mt-5 d-flex justify-content-left">
-        <h3>This is the Employee page.</h3>
+        <Table className="mt-4" striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th> EmployeeID </th>
+              <th> EmployeeName </th>
+              <th> Department </th>
+              <th> MailID </th>
+              <th> DOJ </th>
+              <th> Option </th>
+            </tr>
+          </thead>
+          <tbody>
+            {emps.map(emp => (
+              <tr key={emp.EmployeeID}>
+                <td>{emp.EmployeeID}</td>
+                <td>{emp.EmployeeName}</td>
+                <td>{emp.Department}</td>
+                <td>{emp.MailID}</td>
+                <td>{emp.DOJ}</td>
+                <td>
+                  <ButtonToolbar>
+                    <Button
+                      className="mr-2"
+                      variant="info"
+                      onClick={() =>
+                        this.setState({
+                          editModalShow: true,
+                          empid: emp.EmployeeID,
+                          empname: emp.EmployeeName,
+                          department: emp.Department,
+                          mailid: emp.MailID,
+                          doj: emp.DOJ
+                        })
+                      }
+                    >
+                      Edit
+                    </Button>
+
+                    <Button
+                      className="mr-2"
+                      onClick={() => this.deleteDep(emp.EmployeeID)}
+                      variant="danger"
+                    >
+                      Delete
+                    </Button>
+
+                    <EditEmpModal
+                      show={this.state.editModalShow}
+                      onHide={editModalClose}
+                      empid={empid}
+                      empname={empname}
+                      department={department}
+                      mailid={mailid}
+                      doj={doj}
+                    />
+                  </ButtonToolbar>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+
+        <ButtonToolbar>
+          <Button
+            variant="primary"
+            onClick={() => this.setState({ addModalShow: true })}
+          >
+            Add Employee
+          </Button>
+          <AddEmpModal show={this.state.addModalShow} onHide={addModalClose} />
+        </ButtonToolbar>
       </div>
     );
   }
