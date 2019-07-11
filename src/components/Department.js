9fbda9c62d5ad57a3ab5eea72ccb24a100f44800
ken <kenchen@pneumasoftware.com>
2019-07-11 16:23:12 +0800

    Edit function

--- src/components/Department.js
+++ src/components/Department.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import { Table, Button, ButtonToolbar, ButtonGroup } from "react-bootstrap";
 import { AddDepModal } from "./AddDepModal";
+import { EditDepModal } from "./EditDepModal";
 
 export class Department extends Component {
   constructor(props) {
     super(props);
-    this.state = { deps: [], addModalShow: false };
+    this.state = { deps: [], addModalShow: false, editModalShow: false };
   }
 
   componentDidMount() {
@@ -25,8 +26,9 @@ export class Department extends Component {
   }
 
   render() {
-    const { deps } = this.state;
+    const { deps, depid, depname } = this.state;
     let addModalClose = () => this.setState({ addModalShow: false });
+    let editModalClose = () => this.setState({ editModalShow: false });
 
     return (
       <div>
@@ -35,6 +37,7 @@ export class Department extends Component {
             <tr>
               <th> DepartmentID </th>
               <th> DepartmentName </th>
+              <th> Option </th>
             </tr>
           </thead>
           <tbody>
@@ -42,6 +45,29 @@ export class Department extends Component {
               <tr key={dep.DepartmentID}>
                 <td>{dep.DepartmentID}</td>
                 <td>{dep.DepartmentName}</td>
+                <td>
+                  <ButtonToolbar>
+                    <Button
+                      className="mr-2"
+                      variant="info"
+                      onClick={() =>
+                        this.setState({
+                          editModalShow: true,
+                          depid: dep.DepartmentID,
+                          depname: dep.DepartmentName
+                        })
+                      }
+                    >
+                      Edit
+                    </Button>
+                    <EditDepModal
+                      show={this.state.editModalShow}
+                      onHide={editModalClose}
+                      depid={depid}
+                      depname={depname}
+                    />
+                  </ButtonToolbar>
+                </td>
               </tr>
             ))}
           </tbody>
