e2b0febe92465a33ae79c97342049bcd5b301e51
ken <kenchen@pneumasoftware.com>
2019-07-11 12:16:49 +0800

    Add pop up

--- src/components/Department.js
+++ src/components/Department.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-import { Table } from "react-bootstrap";
+import { Table, Button, ButtonToolbar, ButtonGroup } from "react-bootstrap";
+import { AddDepModal } from "./AddDepModal";
 
 export class Department extends Component {
   constructor(props) {
     super(props);
-    this.state = { deps: [] };
+    this.state = { deps: [], addModalShow: false };
   }
 
   componentDidMount() {
@@ -21,24 +22,37 @@ export class Department extends Component {
 
   render() {
     const { deps } = this.state;
+    let addModalClose = () => this.setState({ addModalShow: false });
 
     return (
-      <Table className="mt-4" striped bordered hover size="sm">
-        <thead>
-          <tr>
-            <th> DepartmentID </th>
-            <th> DepartmentName </th>
-          </tr>
-        </thead>
-        <tbody>
-          {deps.map(dep => (
-            <tr key={dep.DepartmentID}>
-              <td>{dep.DepartmentID}</td>
-              <td>{dep.DepartmentName}</td>
+      <div>
+        <Table className="mt-4" striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th> DepartmentID </th>
+              <th> DepartmentName </th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {deps.map(dep => (
+              <tr key={dep.DepartmentID}>
+                <td>{dep.DepartmentID}</td>
+                <td>{dep.DepartmentName}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+
+        <ButtonToolbar>
+          <Button
+            variant="primary"
+            onClick={() => this.setState({ addModalShow: true })}
+          >
+            Add Department
+          </Button>
+          <AddDepModal show={this.state.addModalShow} onHide={addModalClose} />
+        </ButtonToolbar>
+      </div>
     );
   }
 }
